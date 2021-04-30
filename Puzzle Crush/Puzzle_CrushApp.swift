ba991e142ea4d53030ef8105526122c4c287ac64
callum day ham <caldayham@gmail.com>
2021-04-30 16:28:08 -0700

    Initial Commit

---
+++ Puzzle Crush/Puzzle_CrushApp.swift
@@ -0,0 +1,17 @@
+//
+//  Puzzle_CrushApp.swift
+//  Puzzle Crush
+//
+//  Created by Callum Day Ham on 4/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Puzzle_CrushApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
